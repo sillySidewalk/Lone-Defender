317545a2e748afb6faa5e68e8967191ee25de371
Alex Bailey <alex.bailey@live.com>
2024-07-20 16:57:52 -0700

    changing Map to Manager

---
+++ Assets/Lone-Defender-Code/Manager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    [SerializeField] public List<Clearing> clearings;
+    [SerializeField] public List<Road> roads;
+    [SerializeField] public List<Forest> forests;
+
+}
