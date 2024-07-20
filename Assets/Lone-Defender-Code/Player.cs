8381e2ab102666b224bd7b5a9bd8aff583496ba2
Alex Bailey <alex.bailey@live.com>
2024-07-20 16:57:17 -0700

    adding possible moves

---
+++ Assets/Lone-Defender-Code/Player.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Player : Pawn
+{
+    /*
+     * List valid locations to move to.  
+     */
+    public override List<Location> possible_moves()
+    {
+        
+        if (current_location.GetType() == typeof(Clearing))
+        {
+            // If player is in a clearing, they can move to an ajacent Clearing or Forest
+            return current_location.adjacent_locations;
+        }
+        else if(current_location.GetType() == typeof(Forest))
+        {
+            //If player is in Forest, they can move to adjacent Clearing.
+            return current_location.adjacent_locations.OfType<Clearing>().Cast<Location>().ToList();
+        }
+        else
+        {
+            Console.Error.WriteLine("current_location was unknown type");
+            return null;
+        }    
+    }
+
+    
+
+}
