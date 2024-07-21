16e165589457b3348244981d4bec6cdb79f860d0
Alex Bailey <alex.bailey@live.com>
2024-07-20 17:00:18 -0700

    various small things

--- Assets/Lone-Defender-Code/Location.cs
+++ Assets/Lone-Defender-Code/Location.cs
@@ -7,9 +7,13 @@ using UnityEngine;
  */
 public class Location : MonoBehaviour
 {
-    [SerializeField] int id;
-    [SerializeField] int max_buildings;
-    [SerializeField] List<Building> buildings; // Number of buildings is limited
-    [SerializeField] List<LD_Token> tokens; // Number of tokens is not limited
-    [SerializeField] List<Pawn> pawns; // Pawns are not limited (Probably)
+    [SerializeField] protected int id;
+    [SerializeField] protected int max_buildings;
+    [SerializeField] public List<Building> buildings; // Number of buildings is limited
+    [SerializeField] public List<LD_Token> tokens; // Number of tokens is not limited
+    [SerializeField] public List<Pawn> pawns; // Pawns are not limited (Probably)
+    [SerializeField] public List<Location> adjacent_locations; // Clearings and Forests
+    [SerializeField] public List<Road> adjacent_roads; // slightly different context between forest and clearing, but I think it'll be ok
+
+
 }
