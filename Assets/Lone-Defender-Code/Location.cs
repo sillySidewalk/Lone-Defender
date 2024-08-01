f2bbdebee71fef1ca104944dde86b066aada6426
Alex Bailey <alex.bailey@live.com>
2024-07-31 22:00:10 -0700

    Locations now decide where pawns are put based on their type

--- Assets/Lone-Defender-Code/Location.cs
+++ Assets/Lone-Defender-Code/Location.cs
@@ -10,10 +10,12 @@ using UnityEngine.UIElements;
 public class Location : MonoBehaviour
 {
     [SerializeField] protected int id;
+    [SerializeField] protected Manager man;
     [SerializeField] protected int max_buildings;
-    [SerializeField] public Transform enemy_position; // The position in the location where enemies are put
+    [SerializeField] protected List<Transform> positions; // List of transforms for position references
+    //[SerializeField] public Transform enemy_position; // The position in the location where enemies are put
     [SerializeField] public TextMeshProUGUI enemy_count_txt; // A counter to show how many enemies are in the enemy_position
-    [SerializeField] public Transform player_position; // The position in the location where the player is put
+    //[SerializeField] public Transform player_position; // The position in the location where the player is put
     [SerializeField] public List<Building> buildings = new List<Building>(); // Number of buildings is limited
     [SerializeField] public List<LD_Token> tokens = new List<LD_Token>(); // Number of tokens is not limited
     [SerializeField] public List<Pawn> enemy_pawns = new List<Pawn>(); // Pawns are not limited (Probably)
@@ -22,14 +24,29 @@ public class Location : MonoBehaviour
     [SerializeField] public List<Road> adjacent_roads = new List<Road>(); // slightly different context between forest and clearing, but I think it'll be ok
     public GameObject possible_move_indicator;
 
+    public enum move_position // Which of the move positions to be in, like player or enemy
+    {
+        enemy,
+        player,
+    }
+
     public int get_it()
     {
         return id;
     }
 
+    /*
+     * When a pawn moves to a clearing, Location decides where to put the pawn based on what move_position they have
+     */
+    public void add_pawn(Pawn p)
+    {
+        p.transform.position = positions[(int)p.move_pos].position;
+    }
+
+
     protected void OnMouseDown()
     {
-        Debug.Log("clicked location: " + id);
+        man.location_click(this);
     }
 
 
