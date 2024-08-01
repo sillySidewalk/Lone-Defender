4fee95a2751aa5ddf3194111c7677c7aedece8db
Alex Bailey <alex.bailey@live.com>
2024-07-31 22:04:13 -0700

    adding substate gathering and changing code

--- Assets/Lone-Defender-Code/Manager.cs
+++ Assets/Lone-Defender-Code/Manager.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Transactions;
+using System.Xml;
 using UnityEngine;
 
 public class Manager : MonoBehaviour
@@ -9,9 +11,13 @@ public class Manager : MonoBehaviour
     public List<Clearing> clearings;
     public List<Road> roads;
     public List<Forest> forests;
-    public Dictionary<string, game_state> game_states;
-    public Dictionary<string, sub_state> game_sub_states;
-    public game_state current_sub_state;
+    [SerializeField] protected GameObject game_state_obj;
+    [SerializeField] protected GameObject sub_state_obj;
+    public Dictionary<string, game_state> game_states = new();
+    public Dictionary<string, sub_state> sub_states = new();
+    public sub_state current_sub_state;
+    public game_state current_game_state;
+    protected List<sub_state> loc_clk_sstage_subscription = new List<sub_state>(); // which substates want to be told about a Location click
     public Player player;
     System.Random rnd = new System.Random();
     int dice_value = 10; // The type of dice
@@ -28,6 +34,17 @@ public class Manager : MonoBehaviour
         player_move,
     }
 
+    private void Awake()
+    {
+        init();
+    }
+
+
+    public void init()
+    {
+        init_sub_states();
+        init_game_states();
+    }
 
 
     /*
@@ -121,11 +138,77 @@ public class Manager : MonoBehaviour
         end_cl.enemy_count_txt.text = num.ToString();
     }
 
+
+    /*
+     * For initializing, get the sub_state game object with all the sub_states, then add them to the list of sub_states
+     */
+    protected void init_sub_states()
+    {
+        List<sub_state> sstates = sub_state_obj.GetComponents<sub_state>().ToList();
+
+        foreach(sub_state sstate in sstates)
+        {
+            sub_states.Add(sstate.sub_state_name, sstate);
+
+            // If they want a location click event
+            if(sstate.loc_click_sub)
+            {
+                loc_clk_sstage_subscription.Add(sstate);
+            }
+        }
+
+        // set choose_sub_state as initial state and call it's start
+        current_sub_state = sub_states["choose_sub_state"];
+        current_sub_state.start_state();
+    }
+
+    protected void init_game_states()
+    {
+        List<game_state> gstates = game_state_obj.GetComponents<game_state>().ToList();
+
+        foreach(game_state gstate in gstates)
+        {
+            game_states.Add(gstate.game_state_name, gstate);
+        }
+
+        current_game_state = game_states["player_turn"];
+        current_game_state.start_state();
+    }
+
+
+    /*
+     * When a Location is clicked, it will call this. Then Manager will check if the current sub state wants to be notified, then send over the information
+     */
+    public void location_click(Location loc)
+    {
+        if(loc_clk_sstage_subscription.Contains(current_sub_state))
+        {
+            current_sub_state.loc_click(loc);
+        }
+    }
+
+    public void change_sub_state(String sub_state_name)
+    {
+        current_sub_state.end_state();
+        current_sub_state = sub_states[sub_state_name];
+        current_sub_state.start_state();
+    }
+
     private void Update()
     {
-        if (Input.GetKeyDown("1"))
+        if (Input.GetKeyDown("d"))
         {
-            
+            Debug.Log("sub_states");
+            foreach(KeyValuePair<string, sub_state> entry in sub_states)
+            {
+                Debug.Log(entry.Key + " = " + entry.Value);
+            }
+
+            Debug.Log("game_states");
+            foreach (KeyValuePair<string, game_state> entry in game_states)
+            {
+                Debug.Log(entry.Key + " = " + entry.Value);
+            }
         }
     }
 
