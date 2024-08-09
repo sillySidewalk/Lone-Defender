7d72cebd9258f0907aee9d11e0ad29cd2cd5a138
Alex Bailey <alex.bailey@live.com>
2024-08-08 23:03:39 -0700

    giving buildings a folder, adding enemy buildings

---
+++ Assets/Lone-Defender-Code/Buildings/spawn.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawn : Building
+{
+    factory fact; // The factory these units will move towards
+}
