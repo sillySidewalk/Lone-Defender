095cb6378f2bf5def058ad35898841c409d182bb
Alex Bailey <alex.bailey@live.com>
2024-08-08 23:03:39 -0700

    giving buildings a folder, adding enemy buildings

---
+++ Assets/Lone-Defender-Code/Buildings/Building.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : MonoBehaviour
+{
+    public int id;
+    public Location loc; // The location of the buildings
+}
